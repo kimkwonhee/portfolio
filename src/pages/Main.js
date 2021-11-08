111591d7ec5b59fe7713cddd31ca6c25104b22b7
kimkwonhee <kimkwon429@naver.com>
2021-11-08 19:09:57 +0900

    메인 작성중

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 
 const Main = () => {
     return (
-        <>
-            <div>Main</div>
-        </>
+        <Wrapper>
+            <Inner>
+                <div>WEB<br/>PORTFOLIO</div>
+                <div>Front-end Developer</div>
+                <div>2020 - 2021</div>
+                <div>index contects</div>
+                <div>name kimkwonhee</div>
+                <div>hp 010-9321-6734</div>
+                <div>email kimkwon429@naver.com</div>
+            </Inner>
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.div`
+    width : 100%;
+    display : flex;
+    justify-content : center;
+    background : red;
+`
+
+const Inner = styled.div`
+    width : 1200px;
+    height : 100vh;
+    background-color : green;
+`
+
 
 export default Main
