3b1732a071b6d0c42d36037046e6e464373b72d2
kimkwonhee <kimkwon429@naver.com>
2021-11-08 18:06:46 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Main from "./pages/Main";
+import About from "./pages/About";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import styled from "styled-components";
+import Portfolio from "./pages/Portfolio";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Wrapper>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Main />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+        </Routes>
+      </BrowserRouter>
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.div`
+  width : 100%;
+  height : 100vh;
+`
 
 export default App;
