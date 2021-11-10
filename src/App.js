ee4bfbd1c2b85874f9e90807966128ab8b0f2c15
kimkwonhee <kimkwon429@naver.com>
2021-11-10 18:38:21 +0900

    페이지 작업진행중

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from "react";
 import Main from "./pages/Main";
 import About from "./pages/About";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import Portfolio from "./pages/Portfolio";
+
+
+const Globalstyles = createGlobalStyle`
+
+    html, body {
+        font-family: 'Noto Sans KR', sans-serif;
+        font-size : 16px;
+    }
+`
 
 
 const App = () => {
   return (
     <Wrapper>
+      <Globalstyles />
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Main />} />
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,13 +5,20 @@ const Main = () => {
     return (
         <Wrapper>
             <Inner>
-                <div>WEB<br/>PORTFOLIO</div>
-                <div>Front-end Developer</div>
-                <div>2020 - 2021</div>
-                <div>index contects</div>
-                <div>name kimkwonhee</div>
-                <div>hp 010-9321-6734</div>
-                <div>email kimkwon429@naver.com</div>
+                <InnerCenter>
+                    <TitleInner>
+                        <Title>PORTFOLIO</Title>
+                        <SubTitle>Front-end Developer</SubTitle>
+                        <SubYear>2020 - 2021</SubYear>
+                    </TitleInner>
+                    <ConTactInner>
+                        
+                        <Name>KwonHee Kim<br/>김권희</Name><br/>
+                        <Number>+82 010 9321 6734</Number>
+                        <Email>kimkwon429@naver.com</Email>
+
+                    </ConTactInner>
+                </InnerCenter>
             </Inner>
         </Wrapper>
     )
@@ -21,14 +28,60 @@ const Wrapper = styled.div`
     width : 100%;
     display : flex;
     justify-content : center;
-    background : red;
+    /* background : red; */
 `
 
 const Inner = styled.div`
     width : 1200px;
     height : 100vh;
-    background-color : green;
+    display : flex;
+    align-items : center;
+    /* background-color : green; */
 `
+
+const InnerCenter = styled.div`
+    width : 100%;
+    /* background : grey; */
+`
+
+const TitleInner = styled.div`
+`
+
+const Title = styled.div`
+    font-size: 80px;
+    font-weight: 900;
+    line-height: 1.29;
+    color: #3f637c;
+`
+const SubTitle = styled.div`
+    font-size: 28px;
+    font-weight: 600;
+    line-height: 1.29;
+    color: #222;
+`
+
+const SubYear = styled.div`
+    font-size: 16px;
+    font-weight: 600;
+    line-height: 1.29;
+    color: #222;
+`
+
+const ConTactInner = styled.div`
+    margin-top : 400px;
+    float : right;
+`
+
+const Name = styled.div`
+
+`
+const Number = styled.div`
+
+`
+const Email = styled.div`
+
+`
+
 
 
 export default Main
